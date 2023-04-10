d22d9638aed09fd3e15398b2d1be59ca16bca8b3
kimheeseok <kim1140129@gmail.com>
2023-04-10 14:42:10 +0900

    <TEST> : Comment API Test Code추가 1. controller 테스트 코드 추가 2. service 테스트 코드 추가

--- src/api/comment/controller/comment.controller.spec.ts
+++ src/api/comment/controller/comment.controller.spec.ts
@@ -1,18 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentController } from './comment.controller';
+import { CommentService } from '../service/comment.service';
+
+// Mock CommentService
+const mockCommentService = () => ({
+  createComment: jest.fn(),
+  deleteComment: jest.fn(),
+});
 
 describe('CommentController', () => {
-  let controller: CommentController;
+  let commentController: CommentController;
+  let commentService: CommentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentController],
+      providers: [
+        {
+          provide: CommentService,
+          useFactory: mockCommentService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<CommentController>(CommentController);
+    commentController = module.get<CommentController>(CommentController);
+    commentService = module.get<CommentService>(CommentService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(commentController).toBeDefined();
+  });
+
+  describe('댓글 생성 컨트롤러 테스트', () => {
+    it('댓글 생성 정상 케이스 ', async () => {
+      const userId = 1;
+      const feedId = 123;
+      const commentCreateDto = { comment: 'Test comment' };
+      const expectedResult = { id: 1, ...commentCreateDto };
+
+      commentService.createComment = jest.fn().mockResolvedValue(expectedResult);
+
+      const req = { user: userId };
+      expect(
+        await commentController.createComment(req, feedId, commentCreateDto),
+      ).toEqual(expectedResult);
+      expect(commentService.createComment).toHaveBeenCalledWith(
+        userId,
+        feedId,
+        commentCreateDto,
+      );
+    });
+  });
+
+  describe('댓글 삭제 컨트롤러 테스트', () => {
+    it('댓글 삭제 정상 케이스', async () => {
+      const userId = 1;
+      const feedId = 123;
+      const commentId = 1;
+      const expectedResult = '댓글이 삭제 되었습니다.';
+
+      commentService.deleteComment = jest
+        .fn()
+        .mockResolvedValue(expectedResult);
+
+      const req = { user: userId };
+      expect(
+        await commentController.deleteComment(req, feedId, commentId),
+      ).toEqual(expectedResult);
+      expect(commentService.deleteComment).toHaveBeenCalledWith(
+        commentId,
+        userId,
+      );
+    });
   });
 });
--- src/api/comment/service/comment.service.spec.ts
+++ src/api/comment/service/comment.service.spec.ts
@@ -1,18 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentService } from './comment.service';
+import { CommentRepository } from '../comment.repository';
+import { CommentCreateDto } from '../dto/comment.create.dto';
+import { BadRequestException } from '@nestjs/common';
+
+const mockCommentRepository = () => ({
+  createComment: jest.fn(),
+  findComment: jest.fn(),
+  deleteComment: jest.fn(),
+});
 
 describe('CommentService', () => {
-  let service: CommentService;
+  let commentService: CommentService;
+  let commentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentService],
+      providers: [
+        CommentService,
+        {
+          provide: CommentRepository,
+          useFactory: mockCommentRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<CommentService>(CommentService);
+    commentService = module.get<CommentService>(CommentService);
+    commentRepository = module.get<CommentRepository>(CommentRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('댓글 서비스 테스트', () => {
+    expect(commentService).toBeDefined();
+  });
+
+  describe('댓글 생성 API테스트', () => {
+    it('댓글 생성 정상 케이스', async () => {
+      const userId = 1;
+      const feedId = 123;
+      const commentCreateDto = { comment: 'Test comment' };
+      const expectedResult = { id: 1, ...commentCreateDto };
+
+      commentRepository.createComment.mockResolvedValue(expectedResult);
+
+      const result = await commentService.createComment(
+        userId,
+        feedId,
+        commentCreateDto,
+      );
+      expect(result).toEqual(expectedResult);
+      expect(commentRepository.createComment).toHaveBeenCalledWith(
+        userId,
+        feedId,
+        commentCreateDto,
+      );
+    });
+  });
+
+  describe('댓글 삭제 API 테스트', () => {
+    it('댓글이 존재 하지 않는 경우', async () => {
+      const commentId = 1;
+      const userId = 1;
+
+      commentRepository.findComment.mockResolvedValue(null);
+
+      await expect(
+        commentService.deleteComment(commentId, userId),
+      ).rejects.toThrow(BadRequestException);
+      expect(commentRepository.findComment).toHaveBeenCalledWith(commentId);
+    });
+
+    it('본인 댓글이 아닌 경우', async () => {
+      const commentId = 1;
+      const userId = 1;
+      const comment = { user_id: 2 };
+
+      commentRepository.findComment.mockResolvedValue(comment);
+
+      await expect(
+        commentService.deleteComment(commentId, userId),
+      ).rejects.toThrow(BadRequestException);
+      expect(commentRepository.findComment).toHaveBeenCalledWith(commentId);
+    });
+
+    it('댓글 정상 삭제 케이스', async () => {
+      const commentId = 1;
+      const userId = 1;
+      const comment = { user_id: 1 };
+      const expectedResult = '댓글이 삭제 되었습니다.';
+
+      commentRepository.findComment.mockResolvedValue(comment);
+      commentRepository.deleteComment.mockResolvedValue(1);
+
+      const result = await commentService.deleteComment(commentId, userId);
+      expect(result).toEqual(expectedResult);
+      expect(commentRepository.deleteComment).toHaveBeenCalledWith(
+        commentId,
+        userId,
+      );
+    });
   });
 });
